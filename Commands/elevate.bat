af741601e8cf36c5e73c1bf7be3dfddb42842aec
Nett18 <kressckerl@gmail.com>
2017-11-30 20:48:06 +0100

    Fixed the install error that I pushed too fast.

---
+++ Commands/elevate.bat
@@ -0,0 +1,5 @@
+echo This is possibly Work in progress
+echo The thing is: Don't wait for an update
+echo Then request it in my github via an error
+echo Run command 'cmd+' or 'cmdextension'
+exit /b
--- setup.bat
+++ setup.bat
@@ -3,13 +3,16 @@ net session >nul 2>&1
 :: the above is to check for admin rights
 if %errorlevel% == 0 goto continue
 echo No admin rights detected.
+echo %~dp0
 pause
 exit /b
 :continue
 rem my second try to install
+echo %~dp0
 echo Note you can also just add the Commands to PATH and ignore this .bat file
-set /p q3="Are you sure you wan't to install CMDextension in the C:\Windows folder[y/n]: "
-if q3 == y copy Commands\* "C:\Windows" && echo Tried to install it in C:\Windows
+echo Press enter to continue
+pause > nul
+copy "%~dp0Commands\*" "C:\Windows"
 echo Done installing
 pause
 exit /b
