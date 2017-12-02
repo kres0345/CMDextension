3ca0cc9dbc4e1f42a6daeb2b060856e03923a93a
Nett18 <kressckerl@gmail.com>
2017-12-02 15:25:04 +0100

    removed useless stuff

--- setup.bat
+++ setup.bat
@@ -1,16 +1,14 @@
 @echo off
+title CMDextension ^| Installer
 net session >nul 2>&1
 :: the above is to check for admin rights
 if %errorlevel% == 0 goto continue
 echo No admin rights detected.
-echo %~dp0
 pause
 exit /b
 :continue
-rem my second try to install
-echo %~dp0
-echo Note you can also just add the Commands to PATH and ignore this .bat file
-echo Press enter to continue
+echo NOTE: You can also just add the Commands to PATH and ignore this .bat file
+echo Press {enter} to continue, or {close the window} to stop.
 pause > nul
 copy "%~dp0Commands\*" "C:\Windows"
 echo Done installing
