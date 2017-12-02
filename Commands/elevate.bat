bbf24e85eec807819093b1445ef61f51c7b86b79
Nett18 <kressckerl@gmail.com>
2017-12-02 15:28:42 +0100

    Update

--- Commands/elevate.bat
+++ Commands/elevate.bat
@@ -1,3 +1,4 @@
+@echo off
 echo This is possibly Work in progress
 echo The thing is: Don't wait for an update
 echo Then request it in my github via an error
--- setup.bat
+++ setup.bat
@@ -4,9 +4,11 @@ net session >nul 2>&1
 :: the above is to check for admin rights
 if %errorlevel% == 0 goto continue
 echo No admin rights detected.
+echo NOTE: This isnt needed if you want to use the shourtcut.
 pause
 exit /b
 :continue
+echo NOTE: This isnt needed if you want to use the shourtcut.
 echo NOTE: You can also just add the Commands to PATH and ignore this .bat file
 echo Press {enter} to continue, or {close the window} to stop.
 pause > nul
